730b9596d7f67df8b784395fa0f95d685aa0fb74
Tatikonda Lokesh <68372426+lokesh-tatikonda@users.noreply.github.com>
2026-03-27 16:28:12 +0530

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,1 +1,3 @@
+def call(){
 slackSend(color: '#05FA1D', message:"${currentBuild.currentResult}: ${env.BUILD_NUMBER}, Branch Name: ${env.BRANCH_NAME}, URL: ${env.BUILD_URL}", channel:'#all-lokesh') 
+}
